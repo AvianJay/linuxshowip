8ade0f5091025b247e1423123ce01ab878974f1a
AvianJay <48914361+AvianJay@users.noreply.github.com>
2021-03-28 00:00:31 +0800

    Add files via upload

---
+++ ip.sh
@@ -0,0 +1,8 @@
+if [ ! -n "`which screen`" ]; then
+    echo You No Install Screen,Now Installing...
+    apt install screen -y
+echo Showing IP...
+screen -L -Logfile ~/log/se.$(date +%S).log -dmS ipshower /data/data/com.termux/files/usr/bin/bash -c "wget orange.tw && sheep 1 && exit"
+sleep 0.5
+cat index.html
+rm index.html
