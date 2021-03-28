f3b8cb11b8d146658c3f477dcba0b3ba31f0b13f
AvianJay <48914361+AvianJay@users.noreply.github.com>
2021-03-28 18:12:12 +0800

    Fix sleep bugs and if & fi

--- ip.sh
+++ ip.sh
@@ -1,9 +1,10 @@
 if [ ! -n "`which screen`" ]; then
     echo You No Install Screen,Now Installing...
     apt install screen -y
+fi
 echo Showing IP...
-screen -L -Logfile ~/log/se.$(date +%S).log -dmS ipshower /data/data/com.termux/files/usr/bin/bash -c "wget orange.tw && sheep 1 && exit"
-sleep 0.5
+screen -L -Logfile ~/log/se.$(date +%S).log -dmS ipshower /data/data/com.termux/files/usr/bin/bash -c "wget orange.tw && exit"
+sleep 1
 cat index.html
 rm index.html
 exit
