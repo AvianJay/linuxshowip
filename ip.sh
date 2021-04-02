a0f6bab13cd57012048037cf7fdba591a1010550
AvianJay <48914361+AvianJay@users.noreply.github.com>
2021-04-02 10:39:53 +0800

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,9 +1,5 @@
-if [ ! -n "`which screen`" ]; then
-    echo You No Install Screen,Now Installing...
-    apt install screen -y
-fi
 echo Showing IP...
-screen -L -dmS ipshower /data/data/com.termux/files/usr/bin/bash -c "wget orange.tw && exit"
+screen -L -dmS ipshower bash -c "wget orange.tw && exit"
 sleep 1
 cat index.html
 rm index.html
