9a6da9def29b9ddf8d414a91ac66ba141f2859cc
AvianJay <48914361+AvianJay@users.noreply.github.com>
2021-06-11 13:42:39 +0800

    Create installter.sh

---
+++ installter.sh
@@ -0,0 +1,6 @@
+cd ~/../usr/bin
+sudo wget https://raw.githubusercontent.com/AvianJay/linuxshowip/main/ip.sh
+sudo mv ip.sh showip
+sudo chmod +x showip
+echo linuxshowip installed!
+echo type "showip" on terminal to run linuxshowip.
