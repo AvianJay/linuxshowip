6bc6b3c87436c3623bda39b4c976c46fb660d442
AvianJay <48914361+AvianJay@users.noreply.github.com>
2021-06-20 15:43:35 +0800

    add > nul

--- installter.sh
+++ installter.sh
@@ -1,6 +1,7 @@
 cd ~/../usr/bin
-wget https://raw.githubusercontent.com/AvianJay/linuxshowip/main/ip.sh
-mv ip.sh showip
+wget https://raw.githubusercontent.com/AvianJay/linuxshowip/main/ip.sh > nul
+mv ip.sh showip > nul
 chmod +x showip
 echo linuxshowip installed!
 echo type "showip" on terminal to run linuxshowip.
+rm nul
